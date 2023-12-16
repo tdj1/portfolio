e457483f3ceefa31cb8658841d37176140696079
tdj1 <tdj1@st-andrews.ac.uk>
2023-12-16 19:17:26 +0000

    Projects added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import Project1 from './pages/Project1.tsx';
+import Project2 from './pages/Project2.tsx';
+import Project3 from './pages/Project3.tsx';
+import Project4 from './pages/Project4.tsx';
+import Project5 from './pages/Project5.tsx';
+import Project6 from './pages/Project6.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -14,6 +19,11 @@ ReactDOM.createRoot(rootElement!).render(
       <Routes>
         <Route path="/portfolio/" element={<App />} />
         <Route path="/portfolio/project1" element={<Project1 />} />
+        <Route path="/portfolio/project2" element={<Project2 />} />
+        <Route path="/portfolio/project3" element={<Project3 />} />
+        <Route path="/portfolio/project4" element={<Project4 />} />
+        <Route path="/portfolio/project5" element={<Project5 />} />
+        <Route path="/portfolio/project6" element={<Project6 />} />
       </Routes>
     </Router>
   </React.StrictMode>,
